c132100a8f3cd340c8e6fcdd20b1a59b50ab67c7
MI-Optimist <ajureelmi@gmail.com>
2022-01-29 13:49:26 -0500

    function to create HTML page within new module

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
+//Add modules
+const fs = require('fs');
+
 //Get commandline arguments
 const profileDataArgs = process.argv.slice(2,process.argv.length);
 
-//Function to process commandline arguments
-const printProfileData = profileDataArr => {
-    //Old scholl for loop
-    // for (let i=0; i<profileDataArr.length; i++){
-    //     console.log(profileDataArr[i]);
-    // }
+//deconstruct args
+const [name, github] = profileDataArgs;
 
-    //Clean forEach loop - prints to console
-    profileDataArr.forEach((profileItem) =>console.log(profileItem));
+const generatePage =  (userName, githubName) => {
+    return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+    </head>
+  
+    <body>
+      <h1>${name}</h1>
+      <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
+    `;
 }
 
-// call our function to process the commandline arguments
-printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
